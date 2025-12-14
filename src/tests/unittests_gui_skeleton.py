d5d61b2d7f37612e13a59faea8dc4f48fdb109a6
ais-curran <243846395+ais-curran@users.noreply.github.com>
2025-12-14 16:29:56 +0100

    Tests: Add more test cases

--- src/tests/unittests_gui_skeleton.py
+++ src/tests/unittests_gui_skeleton.py
@@ -2,10 +2,11 @@ import os
 import sys
 import unittest
 import customtkinter as ctk
+from datetime import datetime
 
 # Add parent directory to path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.gui.gui_skeleton import ClientCard, RecordCard, RecordManager, ClientDialog, RecordManagementSystem, AirlineDialog
+from src.gui.gui_skeleton import RecordManagementSystem,datetime_to_string, _get_field, ClientDialog, AirlineDialog
 
 
 class TestGUI(unittest.TestCase):
@@ -13,17 +14,25 @@ class TestGUI(unittest.TestCase):
     Test cases for the Graphical user interface.
     """
 
-   # def test_fetch_first_matching_key(self):
+    def test_fetch_first_matching_key(self):
+        """Test the _get_field function to fetch the first matching key from a dictionary."""
+        record = {"ID": 1, "name": "Test Name", "Phone_Number": "5551234"}
+        # Test with the first matching key
+        self.assertEqual(_get_field(record, "ID", "id"), "1")
+        # Test with a fallback key
+        self.assertEqual(_get_field(record, "Name", "name"), "Test Name")
+        # Test with missing keys
+        self.assertEqual(_get_field(record, "Missing"), "")
 
 
-
-  #  def test_datetime_to_string (self):
-
-
+    def test_datetime_to_string (self):
+        """Test the datetime_to_string function to convert datetime to string."""
+        test_dt = datetime(2025, 12, 14, 14, 35, 10)
+        expected_str = "2025-12-14 14:35"
+        self.assertEqual(datetime_to_string(test_dt), expected_str)
 
     def test_app_created(self):
         """Test existence of application"""
-
         app = RecordManagementSystem()
 
         self.assertIsNotNone(app)
@@ -55,14 +64,19 @@ class TestGUI(unittest.TestCase):
         self.assertIsInstance(app.stats_title, ctk.CTkLabel)
         self.assertIsInstance(app.stats_count, ctk.CTkLabel)
 
+    def test_buttons_created(self):
+        """Test buttons have been created"""
+        app = RecordManagementSystem()
 
-    # def test_buttons_created
+        self.assertIn("Client", app.nav_buttons)
+        self.assertIn("Airline", app.nav_buttons)
+        self.assertIn("Flight", app.nav_buttons)
 
+        self.assertIsInstance(app.add_btn, ctk.CTkButton)
 
-
-
-    # def_test_open_dialog
-
+        client_btn = app.nav_buttons["Client"]
+        self.assertEqual(app.current_section, "Client")
+        self.assertNotEqual(client_btn.cget("fg_color"), "transparent")
 
 
 
